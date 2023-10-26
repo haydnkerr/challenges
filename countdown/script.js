7e2b79b78499144fd2bb7c32b35bcfd746866782
haydnkerr <haydn.kerr@uconn.edu>
2023-10-26 17:29:14 -0400

    Initial Commit

---
+++ countdown/script.js
@@ -0,0 +1,85 @@
+
+
+let countDownDate = new Date("Nov 3, 2023 19:00:00").getTime()
+
+let daysLeft = document.getElementById('days')
+let hoursLeft = document.getElementById('hours')
+let minsLeft = document.getElementById('minutes')
+let secsLeft = document.getElementById('seconds')
+
+const d = new Date();
+let time = d.getTime();
+    let difference = countDownDate - time;
+
+
+let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+let mins = Math.floor(((difference % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60)) / (1000 * 60))
+let secs = Math.floor((((difference % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60)) % (1000 * 60)) / 1000)
+
+if (days < 10) {
+    daysLeft.innerHTML = '0' + days
+} else {
+    daysLeft.innerHTML = days
+}
+
+if (hours < 10) {
+    hoursLeft.innerHTML = '0' + hours
+} else {
+    hoursLeft.innerHTML = hours
+}
+
+if (mins < 10) {
+    minsLeft.innerHTML = '0' + mins
+} else {
+    minsLeft.innerHTML = mins
+}
+
+if (secs < 10) {
+    secsLeft.innerHTML = '0' + secs
+} else {
+    secsLeft.innerHTML = secs
+}
+
+
+
+
+
+
+setInterval (function() {
+    const d = new Date();
+let time = d.getTime();
+    let difference = countDownDate - time;
+
+
+let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+let mins = Math.floor(((difference % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60)) / (1000 * 60))
+let secs = Math.floor((((difference % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60)) % (1000 * 60)) / 1000)
+
+if (days < 10) {
+    daysLeft.innerHTML = '0' + days
+} else {
+    daysLeft.innerHTML = days
+}
+
+if (hours < 10) {
+    hoursLeft.innerHTML = '0' + hours
+} else {
+    hoursLeft.innerHTML = hours
+}
+
+if (mins < 10) {
+    minsLeft.innerHTML = '0' + mins
+} else {
+    minsLeft.innerHTML = mins
+}
+
+if (secs < 10) {
+    secsLeft.innerHTML = '0' + secs
+} else {
+    secsLeft.innerHTML = secs
+}
+},1000)
+
+
