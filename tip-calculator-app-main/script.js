69e18f7a047cc4c0606ad53d51758d2423cac2a7
haydnkerr <haydn.kerr@uconn.edu>
2023-12-28 21:32:11 +1100

    updated JS for tip calculator

--- tip-calculator-app-main/script.js
+++ tip-calculator-app-main/script.js
@@ -1,6 +1,9 @@
 let tipBtn = document.querySelectorAll('.tip-btn');
 let billTotalInput = document.getElementById('bill-total-input')
 let numPeopleInput = document.getElementById('total-people-input')
+let customTip = document.getElementById('custom-tip-input')
+let peopleError = document.getElementById('people-error')
+let tipError = document.getElementById('tip-error')
 
 let tipAmountTotalDisplay = document.getElementById('tip-amount-total-display')
 let totalDisplay = document.getElementById('total-display')
@@ -11,8 +14,9 @@ let numPeople = 0
 let billAmount = 0
 let tipAmountPerPerson = 0
 let totalperPerson = 0
-let tipAmountCalc = 0
-let totalCalc = 0
+let tipAmountTotal = 0
+let totalAmountPerson = 0
+
 
 tipBtn.forEach(function (element) {
     element.addEventListener('click', function () {
@@ -28,18 +32,42 @@ billTotalInput.addEventListener('input', updateBillTotal)
 
 numPeopleInput.addEventListener('input', updateNumPeople)
 
+customTip.addEventListener('input', customTipInput)
+
 resetBtn.addEventListener('click', resetValues)
 
 function updateNumPeople() {
     numPeople = this.value
+    if (numPeople == 0) {
+        peopleError.classList.remove('hidden')
+        this.style.border = '2px solid red'
+    } else {
+        peopleError.classList.add('hidden')
+        this.style.border = '2px solid hsl(189, 41%, 97%)'
+    }
     calculateTip()
     console.log(numPeople)
 }
 
+function customTipInput() {
+    for (let i = 0; i < tipBtn.length; i++) {
+        tipBtn[i].classList.remove('active-tip')
+    }
+    tipValue = this.value / 100
+    calculateTip()
+}
+
 function updateBillTotal() {
     billAmount = this.value
+
+    if (billAmount == 0) {
+        tipError.classList.remove('hidden')
+        this.style.border = '2px solid red'
+    } else {
+        tipError.classList.add('hidden')
+        this.style.border = '2px solid hsl(189, 41%, 97%)'
+    }
     calculateTip()
-    console.log(billAmount)
 }
 
 function updateTip(element) {
@@ -52,11 +80,12 @@ function updateTip(element) {
 function calculateTip() {
 
     if (billAmount > 0 && numPeople > 0) {
-        tipAmountCalc = (billAmount * tipValue) / numPeople
-        totalCalc = (billAmount / numPeople) + tipAmountPerPerson
+        tipAmountTotal = (billAmount * tipValue) / numPeople
+        totalAmountPerson = (billAmount / numPeople) + tipAmountTotal
 
-        tipAmountPerPerson = tipAmountCalc.toFixed(2)
-        totalperPerson = totalCalc.toFixed()
+
+        tipAmountPerPerson = parseFloat(tipAmountTotal.toFixed(2))
+        totalperPerson = parseFloat(totalAmountPerson.toFixed(2))
 
         tipAmountTotalDisplay.innerHTML = '$' + tipAmountPerPerson
         totalDisplay.innerHTML = '$' + totalperPerson
@@ -71,4 +100,6 @@ function resetValues() {
     totalperPerson = 0
     tipAmountTotalDisplay.innerHTML = '$' + tipAmountPerPerson
     totalDisplay.innerHTML = '$' + totalperPerson
+    billTotalInput.value = ''
+    numPeopleInput.value = ''
 }
